0f423243899b87de01245df5a266e09a0133f629
Uduak Essien <acushla4real@gmail.com>
2020-07-24 23:00:29 +0100

    Add hurdle race solution

---
+++ ruby/hurdle_race.rb
@@ -0,0 +1,7 @@
+# Given an array of hurdle heights , and an initial maximum height Dan can jump, , determine the minimum number of doses Dan must take to be able to clear all the hurdles in the race.
+
+# For example, if  and Dan can jump  unit high naturally, he must take  doses of potion to be able to jump all of the hurdles.
+
+def hurdleRace(k, height)
+  k < height.max ? height.max - k : 0
+end
