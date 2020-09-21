c31517dfb347fde57f29bacf5dd87aa0ccce18da
Uduak Essien <acushla4real@gmail.com>
2020-09-21 12:13:53 +0100

    add time in words solution

--- ruby/beautiful_triplets.rb
+++ ruby/beautiful_triplets.rb
@@ -1,30 +1,30 @@
 def beautifulTriplets(d, arr)
-    first = arr.first
-    arr = arr.slice(1, arr.size)
-    count = 1
-    res = 0
-    i = 0
-    
-    while i < arr.length
-        if arr[i] - first == d
-            first = arr[i]
-            count += 1
-            
-            if count == 3
-                first = arr.first
-                arr = arr.slice(1, arr.size)
-                count = 1
-                res += 1
-                i = -1
-            end
-        elsif arr[i] - first > d
-            first = arr.first
-            arr = arr.slice(1, arr.size)
-            count = 1
-            i = -1
-        end
-        
-        i += 1
+  first = arr.first
+  arr = arr.slice(1, arr.size)
+  count = 1
+  res = 0
+  i = 0
+
+  while i < arr.length
+    if arr[i] - first == d
+      first = arr[i]
+      count += 1
+
+      if count == 3
+        first = arr.first
+        arr = arr.slice(1, arr.size)
+        count = 1
+        res += 1
+        i = -1
+      end
+    elsif arr[i] - first > d
+      first = arr.first
+      arr = arr.slice(1, arr.size)
+      count = 1
+      i = -1
     end
-    res
+
+    i += 1
+  end
+  res
 end
---
+++ ruby/time_in_words.rb
@@ -0,0 +1,52 @@
+def timeInWords(h, m)
+  word(h) + "o' clock" if m == 0o0
+
+  case m
+  when 0
+    word(h) + " o' clock"
+  when 30
+    'half past ' + word(h)
+  when 15
+    'quarter past ' + word(h)
+  when 45
+    'quarter to ' + word(h + 1)
+  when 1
+    'one minute past ' + word(h)
+  when 2..29
+    word(m) + ' minutes past ' + word(h)
+  when 31..59
+    word(60 - m) + ' minutes to ' + word(h + 1)
+  end
+end
+
+def word(num)
+  words_hash = { 0 => 'zero',
+                 1 => 'one',
+                 2 => 'two', 3 => 'three',
+                 4 => 'four',
+                 5 => 'five',
+                 6 => 'six',
+                 7 => 'seven',
+                 8 => 'eight',
+                 9 => 'nine',
+                 10 => 'ten',
+                 11 => 'eleven',
+                 12 => 'twelve',
+                 13 => 'thirteen',
+                 14 => 'fourteen',
+                 15 => 'fifteen',
+                 16 => 'sixteen',
+                 17 => 'seventeen',
+                 18 => 'eighteen',
+                 19 => 'nineteen',
+                 20 => 'twenty',
+                 30 => 'thirty',
+                 40 => 'forty',
+                 50 => 'fifty' }
+  if words_hash[num]
+    words_hash[num]
+  else
+    s = num.to_s.split('')
+    words_hash[(s[0] + '0').to_i] + ' ' + words_hash[s[1].to_i]
+  end
+end
