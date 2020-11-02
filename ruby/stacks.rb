bde6da3e1cab3fbe0f7745e75f79a0b875b353e6
Uduak Essien <acushla4real@gmail.com>
2020-11-02 11:07:00 +0100

    add stacks challenge

---
+++ ruby/stacks.rb
@@ -0,0 +1,63 @@
+# Start with your code from LinkedList challenge.
+class Node 
+    attr_accessor :value, :next_node
+      
+    def initialize(value, next_node = nil)
+        @value = value
+      @next_node = next_node
+    end
+  end
+  
+  class Stack
+    attr_accessor :first, :last, :size
+    
+    def initialize
+      @first = nil
+      @last = nil
+      @size = 0
+    end
+  
+    def push(number)
+      new_node = Node.new(number)
+      
+      if @first.nil?
+        @first = new_node
+        @last = new_node
+      else
+        temp = @first
+        @first = new_node
+        @first.next_node = temp
+      end
+      @size += 1
+    end
+    
+    def pop
+      return nil if @first.nil?
+      
+      temp = @first
+      if @first == @last 
+        @last = nil
+      end
+      @first = @first.next_node
+      @size -= 1 
+      
+      temp.value
+    end
+  end
+  
+  stack = Stack.new
+  stack.push(3)
+  stack.push(5)
+  puts stack.pop
+  # => 5
+  
+  stack.push(2)
+  stack.push(7)
+  puts stack.pop
+  # => 7
+  
+  puts stack.pop
+  # => 2
+  
+  puts stack.pop
+  # => 3
