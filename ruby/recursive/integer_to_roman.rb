ea44f3a5b244e8735b3b48aa44ae030af2303da0
Uduak Essien <acushla4real@gmail.com>
2021-01-05 12:46:16 +0100

    Add integer to roman recursive mappings

---
+++ ruby/recursive/integer_to_roman.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+roman_mapping = {
+  1000 => 'M',
+  900 => 'CM',
+  500 => 'D',
+  400 => 'CD',
+  100 => 'C',
+  90 => 'XC',
+  50 => 'L',
+  40 => 'XL',
+  10 => 'X',
+  9 => 'IX',
+  5 => 'V',
+  4 => 'IV',
+  1 => 'I'
+}
+
+def integer_to_roman(roman_mapping, number, result = '')
+  return result if number.zero?
+
+  roman_mapping.each_key do |divisor|
+    quotient, modulus = number.divmod(divisor)
+    result << roman_mapping[divisor] * quotient
+    return integer_to_roman(roman_mapping, modulus, result) if quotient.positive?
+  end
+end
+
+puts integer_to_roman(roman_mapping, 380)
